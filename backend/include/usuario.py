554ab71808ebeed6cc23a3c335c6e3a246c04a56
Fabrizzio Vilchez <115495332+Fabrizzio20k@users.noreply.github.com>
2024-07-15 18:30:50 -0500

    Add requirements

--- backend/include/usuario.py
+++ backend/include/usuario.py
@@ -1,11 +1,12 @@
 from operacion import Operacion
+from datetime import datetime
 
 class Usuario:
-    def __init__(self, numero:str, saldo:float, NumerosContacto: list):
+    def __init__(self, numero:str, saldo:float, NumerosContacto: list[str]):
         self.numero = numero
         self.saldo = saldo
         self.NumerosContacto = NumerosContacto
-        self.historialOperaciones: list[Operacion] = []
+        self.historialOperaciones = []
     
     def __str__(self):
         return f"Numero: {self.numero}, Saldo: {self.saldo}, Numeros de Contacto: {self.NumerosContacto}"
@@ -14,7 +15,16 @@ class Usuario:
         return f"Numero: {self.numero}, Saldo: {self.saldo}, Numeros de Contacto: {self.NumerosContacto}"
     
     def historialOperaciones(self):
-        pass
+        return self.historialOperaciones
 
     def transferir(self, numero:str, monto:float):
-        pass
+        if numero not in self.NumerosContacto:
+            return False
+        if self.saldo < monto:
+            return False
+        self.saldo -= monto
+
+        op = Operacion(self.numero, numero, monto, str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+        self.historialOperaciones.append(op)
+
+        return True
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status, Depends
+from pydantic import BaseModel
+from typing import Optional
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
 
 app = FastAPI()
 
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+class Usuario(BaseModel):
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    nombre = Column(String, index=True)
+    apellido = Column(String, index=True)
+    edad = Column(Integer)
+
+Base.metadata.create_all(bind=engine)
+
+class Usuario(BaseModel):
+    id: Optional[int] = None
+    nombre: str
+    apellido: str
+    edad: int
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
